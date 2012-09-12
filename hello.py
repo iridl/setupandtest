371742e6f148c74022b7b882fc9d63c25790ed7d
Arthur M. Greene <amg@iri.columbia.edu>
2012-09-12 19:12:41 -0400

    No comment

--- hello.py
+++ hello.py
@@ -1,3 +1,13 @@
+""" This is a python script. Execute it by typing
+
+'python hello.py' (without quotes).
+
+The only necessary line is the one beginning with 'Hello.'
+
+This is a 'docstring'. See python.org for (many) more details.
+
+"""
+
 print
 print 'Hello Earthlings!'
 print
